b99e5a325b9e92cc92b55536b4649b40a267dfcd
Edson Rodrigues <itu.edson@yahoo.com.br>
2021-09-28 03:16:11 -0300

    Segundo commit deste projeto.

--- App.js
+++ App.js
@@ -7,15 +7,21 @@ import logo from './assets/shopperWoman.jpg'
 function App(){
 return(
   <View>
-    <Text style={styles.titulo}><FontAwesome name="user-circle" size={50} />Bom dia Vietnã!</Text>
-    <FontAwesome.Button name="shopping-cart" style={styles.botao} >Comprar</FontAwesome.Button> 
+    <Text style={styles.titulo} > Sejam todos benvindos à nossa loja.</Text>
+
     <Image source={logo} style={styles.imagem} />
+   
+    <FontAwesome.Button name="shopping-cart" style={styles.botao} >Adicionar ao carrinho</FontAwesome.Button> 
+    
+     
+
+     <Text style={styles.titulo}><FontAwesome name="heart" size={50} color="pink" />Desejamos boas compras . Divirtam-se!!!!!</Text>
   </View>
 )
 }
 const styles = StyleSheet.create({
   imagem: {
-    width: '90%',
+    width: '75%',
     height: 300,
     resizeMode: 'center'
   }, botao: {
